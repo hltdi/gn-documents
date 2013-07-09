8c6ecddc072e65496f88969c71d14e75dcc0dde5
Alex Rudnick <alex.rudnick@gmail.com>
2013-07-09 16:53:15 -0400

    plugging the mockup HTML into Bottle

--- serverside/urldispatch.py
+++ serverside/urldispatch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+from bottle import abort
+from bottle import error
+from bottle import request, redirect, response
 from bottle import route
 from bottle import static_file
-from bottle import request, redirect, response
 from bottle import template
-from bottle import abort
 
 """
 This module handles all of the URL dispatching for gn-documents, mapping from
@@ -15,4 +16,24 @@ import urllib.request
 
 @route('/')
 def index():
-    return "oh hecks yes"
+    redirect("/inicio")
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
+@route('/catalogo')
+def catalogo():
+    return static_file('catalogo.html', root='static')
+
+@route('/inicio')
+def inicio():
+    return static_file('inicio.html', root='static')
+
+@route('/subir')
+def subir():
+    return static_file('subir.html', root='static')
+
+@route('/style/<fn>')
+def style(fn):
+    return static_file(fn, root='static/style')
