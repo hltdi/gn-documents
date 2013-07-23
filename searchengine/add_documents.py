b9eef3632c7dfa47e141659dff6c05dc0e28d8cc
Alex Rudnick <alex.rudnick@gmail.com>
2013-07-23 17:05:46 -0400

    hooking up tika

--- searchengine/add_documents.py
+++ searchengine/add_documents.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from subprocess import Popen, PIPE
 
 import whoosh.index
 from whoosh.fields import *
@@ -9,12 +10,26 @@ from whoosh.fields import *
 from start_index import THESCHEMA 
 from start_index import INDEXDIR 
 
+RICHFILETYPES = {'doc','docx','pdf'}
+
+def extract_with_tika(fn):
+    process = Popen(['java', '-jar', 'lib/tika-app-1.4.jar', '-t', fn],
+                    stdout=PIPE)
+    (stdout, stderr) = process.communicate()
+    return stdout.decode('utf-8')
+
 def add_document(fn, writer):
     fn = os.path.abspath(fn)
     pathto, endfn = os.path.split(fn)
     text = ""
-    with open(fn) as infile:
-        text = infile.read()
+
+    splitted = fn.rsplit('.')
+    if splitted[-1].lower() in RICHFILETYPES:
+        text = extract_with_tika(fn)
+    else:
+        with open(fn) as infile:
+            text = infile.read()
+
     writer.add_document(title=endfn,
                         path=endfn,
                         tags="foo,bar",
